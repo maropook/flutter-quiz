90e10def3ced268e13ae4c4982337f1e74e51efc
maropook <ii440092@icloud.com>
2021-11-18 11:27:19 +0900

    答えを連続して送信できるバグ修正

--- lib/view/quiz_app.dart
+++ lib/view/quiz_app.dart
@@ -51,6 +51,8 @@ class MainQuizApp extends StatefulWidget {
 class MainQuizAppState extends State<MainQuizApp> {
   late List<Quiz> quizList;
   int index = 0;
+  int result = 0;
+  bool isSelectNow = true;
 
   @override
   void initState() {
@@ -58,7 +60,17 @@ class MainQuizAppState extends State<MainQuizApp> {
     super.initState();
   }
 
-  Future<void> updateQuiz(BuildContext context) async {
+  Future<void> updateQuiz(BuildContext context, int selectAnswer) async {
+    setState(() {
+      isSelectNow = false;
+    });
+    if (quizList[index].answer == selectAnswer) {
+      result++;
+    }
+
+    await Future.delayed(const Duration(seconds: 1));
+    isSelectNow = true;
+    setState(() {});
     index++;
     if (index == quizList.length) {
       await goToResult(context);
@@ -67,37 +79,60 @@ class MainQuizAppState extends State<MainQuizApp> {
   }
 
   Future<void> goToResult(BuildContext context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Result()));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Result(result, quizList.length)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: index < quizList.length
-          ? Container(
+          ? Center(
               child: Column(
               children: [
                 Text(quizList[index].question),
                 TextButton(
                     onPressed: () async {
-                      updateQuiz(context);
+                      if (!isSelectNow) return;
+                      await updateQuiz(context, 1);
                     },
-                    child: Text(quizList[index].select1)),
+                    child: isSelectNow
+                        ? Text(quizList[index].select1)
+                        : quizList[index].answer == 1
+                            ? Text(quizList[index].select1 + "○")
+                            : Text(quizList[index].select1 + "×")),
                 TextButton(
                     onPressed: () async {
-                      updateQuiz(context);
+                      if (!isSelectNow) return;
+                      await updateQuiz(context, 2);
                     },
-                    child: Text(quizList[index].select2)),
+                    child: isSelectNow
+                        ? Text(quizList[index].select2)
+                        : quizList[index].answer == 2
+                            ? Text(quizList[index].select2 + "○")
+                            : Text(quizList[index].select2 + "×")),
                 TextButton(
                     onPressed: () async {
-                      updateQuiz(context);
+                      if (!isSelectNow) return;
+                      await updateQuiz(context, 3);
                     },
-                    child: Text(quizList[index].select3)),
+                    child: isSelectNow
+                        ? Text(quizList[index].select3)
+                        : quizList[index].answer == 3
+                            ? Text(quizList[index].select3 + "○")
+                            : Text(quizList[index].select3 + "×")),
                 TextButton(
                     onPressed: () async {
-                      updateQuiz(context);
+                      if (!isSelectNow) return;
+                      await updateQuiz(context, 4);
                     },
-                    child: Text(quizList[index].select4))
+                    child: isSelectNow
+                        ? Text(quizList[index].select4)
+                        : quizList[index].answer == 4
+                            ? Text(quizList[index].select4 + "○")
+                            : Text(quizList[index].select4 + "×")),
               ],
             ))
           : Container(),
@@ -106,24 +141,48 @@ class MainQuizAppState extends State<MainQuizApp> {
 }
 
 class Result extends StatelessWidget {
-  Result({Key? key}) : super(key: key);
-
+  Result(this.result, this.quizNumber, {Key? key}) : super(key: key);
+  int result;
+  int quizNumber;
+  late String comment;
   @override
   Widget build(BuildContext context) {
+    switch (result * 10 ~/ quizNumber) {
+      case 6:
+        comment = "まあまあ";
+        break;
+      case 7:
+        comment = "まあまあ";
+        break;
+      case 8:
+        comment = "いいね";
+        break;
+      case 9:
+        comment = "すごい";
+        break;
+      case 10:
+        comment = "よくできました";
+        break;
+      default:
+        comment = "頑張りましょう";
+        break;
+    }
+    print("${result / quizNumber * 10}");
+
     return Scaffold(
       body: Center(
         child: Column(
           //Columnの中に入れたものは縦に並べられる．Rowだと横に並べられる
           mainAxisAlignment: MainAxisAlignment.center, //Coloumの中身を真ん中に配置
           children: <Widget>[
-            const Text(
-              'ボタンを押せ',
+            Text(
+              comment,
             ),
             ElevatedButton(
                 onPressed: () {
                   //クイズアプリへ遷移するQuizApp関数がよばれる
                 },
-                child: const Text('リザルト')),
+                child: Text('$result')),
           ],
         ),
       ),
